dbae3f62d67233d3af47b3926064a20ec5b6b4a7
golomolzina <golomolzina@gmail.com>
2021-03-03 21:54:07 +0700

    ТАблицы , наконец, стало видно.

--- tables/shops.sql
+++ tables/shops.sql
@@ -3,11 +3,3 @@ create table Shops(
     name varchar(20)
 
 );
-CREATE TABLE Customers
-(
-    Id SERIAL PRIMARY KEY,
-    FirstName CHARACTER VARYING(30),
-    LastName CHARACTER VARYING(30),
-    Email CHARACTER VARYING(30),
-    Age INTEGER
-)
